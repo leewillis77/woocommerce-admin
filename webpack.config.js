35263d00641334690799b6a2163608d1842d0466
Joshua Flowers <joshuatf@gmail.com>
2020-12-30 19:00:21 -0500

    Use babel to compile debug package

--- webpack.config.js
+++ webpack.config.js
@@ -116,7 +116,7 @@ const webpackConfig = {
 			},
 			{
 				test: /\.js?$/,
-				exclude: /node_modules/,
+				exclude: /node_modules(\/|\\)(?!(debug))/,
 				use: {
 					loader: 'babel-loader',
 					options: {
